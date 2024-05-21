1b0c764cbe867a81b6908e23ea71ed23bf38d3c3
Jordan <olivier.jordan21@gmail.com>
2024-05-21 15:38:15 +0200

    add background and make slot for tools

--- app/screens/GameScreen.tsx
+++ app/screens/GameScreen.tsx
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Stack } from 'expo-router';
-import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Image, ImageBackground } from 'react-native';
 
 import toolsData from '~/app/data/tools.json';
 import blocsData from '~/app/data/bloc';
-
-
 import { Container } from '~/components/Container';
 import { Countdown } from '~/components/Countdown';
 import { saveScore, getScores } from '../utils/scoreManager';
@@ -18,7 +16,6 @@ interface Bloc {
 }
 
 export default function GameScreen() {
-  console.log('in');
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
   const [gameOver, setGameOver] = useState(false);
@@ -51,18 +48,22 @@ export default function GameScreen() {
 
   const handlePress = (toolId: number) => {
     if (gameOver) return;
-    
+
     const topBloc = blocs[0];
     if (toolId === topBloc.toolsId) {
       setBlocs(blocs.slice(1));
       setScore(score + 1);
+
+      if(blocs.length < 6) {
+        setBlocs(generateBlocs(0, 6));
+      }
     } else {
       console.log("Outil incorrect! Essaie encore.");
     }
   };
 
   return (
-    <>
+    <ImageBackground source={require('../../assets/bg.jpg')} style={styles.backgroundImage}>
       <Stack.Screen options={{ title: 'Game' }} />
       <Container>
         <Countdown initialCount={10} onEnd={handleGameOver} />
@@ -79,18 +80,23 @@ export default function GameScreen() {
           {toolsData.map((tool) => (
             <TouchableOpacity
               key={tool.id}
-              style={styles.button}
+              style={[styles.button, styles[`slot${tool.slot}`]]}
               onPress={() => handlePress(tool.id)}>
               <Text>{tool.name}</Text>
             </TouchableOpacity>
           ))}
         </View>
       </Container>
-    </>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
   blocsContainer: {
     flexDirection: 'column',
     alignItems: 'center',
@@ -112,7 +118,9 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   button: {
-    backgroundColor: '#ccc',
+    backgroundColor: 'rgba(204, 204, 204, 0.7)',
+    borderWidth: 3,
+    borderColor: 'rgba(204, 204, 204, 0.7)',
     padding: 10,
     minWidth: 100,
     minHeight: 100,
@@ -127,4 +135,40 @@ const styles = StyleSheet.create({
   rightButton: {
     marginRight: 10,
   },
+  slot1: {
+    padding: 10,
+    minWidth: 100,
+    minHeight: 100,
+    borderRadius: 100,
+    position: 'absolute',
+    left: 10,
+    bottom: 120,
+  },
+  slot2: {
+    padding: 10,
+    minWidth: 100,
+    minHeight: 100,
+    borderRadius: 100,
+    position: 'absolute',
+    right: 10,
+    bottom: 120,
+  },
+  slot3: {
+    padding: 10,
+    minWidth: 100,
+    minHeight: 100,
+    borderRadius: 100,
+    position: 'absolute',
+    left: 10,
+    bottom: 10,
+  },
+  slot4: {
+    padding: 10,
+    minWidth: 100,
+    minHeight: 100,
+    borderRadius: 100,
+    position: 'absolute',
+    right: 10,
+    bottom: 10,
+  }
 });
