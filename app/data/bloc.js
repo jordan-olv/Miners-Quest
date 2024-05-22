83fd34d47b9f450e1b5ebca42a7bf6d219a9d904
Jordan <olivier.jordan21@gmail.com>
2024-05-22 15:32:16 +0200

    edit tools

--- app/data/bloc.js
+++ app/data/bloc.js
@@ -2,31 +2,31 @@ const blocs = [
   {
     id: 1,
     name: 'Dirt',
-    image: require('../../assets/bloc/dirt.webp'), // Assure-toi que le chemin est correct
+    image: require('../../assets/bloc/dirt.webp'),
     toolsId: 3,
   },
   {
     id: 2,
     name: "Haybale",
-    image: require('../../assets/bloc/haybale.png'), // Assure-toi que le chemin est correct
+    image: require('../../assets/bloc/haybale.png'),
     toolsId: 4,
   },
   {
     id: 3,
     name: "Stone",
-    image: require('../../assets/bloc/stone.webp'), // Assure-toi que le chemin est correct
+    image: require('../../assets/bloc/stone.webp'),
     toolsId: 1,
   },
   {
     id: 4,
     name: "Sand",
-    image: require('../../assets/bloc/sand.jpeg'), // Assure-toi que le chemin est correct
+    image: require('../../assets/bloc/sand.jpeg'),
     toolsId: 3,
   },
   {
     id: 5,
     name: "Wood",
-    image: require('../../assets/bloc/wood.webp'), // Assure-toi que le chemin est correct
+    image: require('../../assets/bloc/wood.webp'),
     toolsId: 2,
   }
 ];
---
+++ app/data/tools.js
@@ -0,0 +1,28 @@
+const tools = [
+  {
+    id: 1,
+    name: "Pickaxe",
+    image: require('../../assets/tools/pickaxe.webp'),
+    slot: 1
+  },
+  {
+    id: 2,
+    name: "Axe",
+    image: require('../../assets/tools/axe.webp'),
+    slot: 2
+  },
+  {
+    id: 3,
+    name: "Shovel",
+    image: require('../../assets/tools/shovel.webp'),
+    slot: 3
+  },
+  {
+    id: 4,
+    name: "Hoe",
+    image: require('../../assets/tools/hoe.webp'),
+    slot: 4
+  }
+];
+
+export default tools;
